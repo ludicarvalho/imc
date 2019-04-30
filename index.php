bf433e5c2445b249701f732b2b407aa11c1da449
Ludi Carvalho <40408321+ludicarvalho@users.noreply.github.com>
2019-04-30 02:16:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include "class.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="shortcut icon" href="icone.png" />
     <link rel="stylesheet" href="style.css?v=1" />
     <title>IMC - Indice de massa corporea</title>
 </head>
@@ -24,7 +25,7 @@ include "class.php";
             <label for="at">Altura:</label>
         </div>
         <div class="lbDois">
-            <input type="number" name="altura" class="config" id="at" min="0" max="5" step="0.01" placeholder="Metros" required value="<?php echo $_POST['altura']; ?>" />
+            <input type="number" name="altura" class="config" id="at" min="0" max="3" step="0.01" placeholder="Metros" autofocus required value="<?php echo $_POST['altura'] ?? ""; ?>" />
         </div>
     </div>
 
@@ -33,35 +34,36 @@ include "class.php";
             <label for="ps" class="lb">Peso:</label>
         </div>
         <div class="lbDois">
-            <input type="number" name="peso" class="config" id="ps" min="0" max="999" step="0.01" placeholder="Kg" required value="<?php echo $_POST['peso']; ?>" />
+            <input type="number" name="peso" class="config" id="ps" min="0" max="400" step="0.01" placeholder="Kg" required value="<?php echo $_POST['peso'] ?? ""; ?>" />
         </div>
     </div>
 
     <div class="mb-3">
-        <input type="submit" class="config botao" value="Calcular" />
+        <input type="submit" class="config botao" onclick="imc.validar()" value="Calcular" />
     </div>
 <?php
 if (!empty($_POST)) {
-    $pc = new Imc;
-    $pc->altura = $_POST['altura'];
-    $pc->peso = $_POST['peso'];
+    $a = $_POST['altura'];
+    $p = $_POST['peso'];
+    $pc = new Imc($p,$a);
     $resultado = $pc->calcular();
 ?>
     <div class="mb-3">
-        <hr/>
-        <label for="" class="ps">
+        
+        <p class="ps">
             <?php
-                echo "\t\t\t"
-                     . $resultado
+                echo $resultado
                      ."\n";
             ?>
-        </label>
+        </p>
     </div>
 <?php
 }
 ?>
     </form>
 </div>
+
+<script src="java.js"></script>
 
 </body>
 </html>
